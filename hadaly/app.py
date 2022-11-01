657bf039e0710a4e4e103d9f5e317233fca3b53f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 06:27:07 +0000

    Adding tarfile member sanitization to extractall()

--- hadaly/app.py
+++ hadaly/app.py
@@ -117,7 +117,26 @@ class HadalyApp(App):
                     if file.name.startswith(('..', '/')):
                         raise ValueError
 
-                tar.extractall(path=self.tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=self.tempdir)
         except ValueError as msg:
             Logger.debug('Application: {msg}'.format(msg=msg))
             self.show_popup(_('Error'), _('File is not valid.'))
